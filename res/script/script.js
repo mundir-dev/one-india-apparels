c061209361e32e5ee55694ba1eff1768e0c4d77e
Your Name <you@example.com>
2022-01-21 18:57:37 +0530

    loading screen added

--- res/script/script.js
+++ res/script/script.js
@@ -2,6 +2,10 @@
  AOS.init();
 
  $(document).ready(function(){
+    window.onload = () => {
+        console.log(1, $('.loading-screen'));
+        $('.loading-screen').remove();
+    }
     $(".owl-carousel").owlCarousel({
         dots: false,
         margin: 10,
